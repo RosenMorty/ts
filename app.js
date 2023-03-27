4c592c46a4999d4e34c011c68bae018347d77ab3
Владислав <rolit0852@gmail.com>
2023-03-27 16:34:23 +0300

    continue

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 "use strict";
+// let info: {
+//     officeId: number;
+//     isOpened: boolean;
+//     contacts: {
+//         phone: string;
+//         email: string;
+//         address: {
+//             city: string;
+//         }
+//     }
+// } = {
+//     "officeId": 45,
+//     "isOpened": false,
+//     "contacts": {
+//         "phone": "+79102833223",
+//         "email": "my@email.ru",
+//         "address": {
+//             "city": "Москва"
+//         }
+//     }
+// }
+var PaymentStatus;
+(function (PaymentStatus) {
+    PaymentStatus["Success"] = "success";
+    PaymentStatus["Failed"] = "failed";
+})(PaymentStatus || (PaymentStatus = {}));
--- app.ts
+++ app.ts
@@ -1,21 +1,80 @@
-let info: {
-    officeId: number;
-    isOpened: boolean;
-    contacts: {
-        phone: string;
-        email: string;
-        address: {
-            city: string;
-        }
-    }
-} = {
-    "officeId": 45,
-    "isOpened": false,
-    "contacts": {
-        "phone": "+79102833223",
-        "email": "my@email.ru",
-        "address": {
-            "city": "Москва"
-        }
-    }
+// let info: {
+//     officeId: number;
+//     isOpened: boolean;
+//     contacts: {
+//         phone: string;
+//         email: string;
+//         address: {
+//             city: string;
+//         }
+//     }
+// } = {
+//     "officeId": 45,
+//     "isOpened": false,
+//     "contacts": {
+//         "phone": "+79102833223",
+//         "email": "my@email.ru",
+//         "address": {
+//             "city": "Москва"
+//         }
+//     }
+// }
+
+// enum QuestionStatus {
+//     Pubblished = 'published',
+//     Draft = 'draft',
+//     Deleted = 'deleted',
+// }
+
+// async function getFaqs(req:{
+//     topicId: number,
+//     status: QuestionStatus,
+// }): Promise<{
+//     question: string;
+//     answer: string;
+//     tags: string[];
+//     likes: number;
+//     status: QuestionStatus;
+// }[]> {
+//     const res = await fetch('/faqs', {
+//         method: 'POST',
+//         body: JSON.stringify(req)
+//     });
+//     const data = await res.json();
+//     return data;
+// }
+
+interface Ipayment {
+    sum: number,
+    from: number,
+    to: number
 }
+
+enum PaymentStatus {
+    Success = 'success',
+    Failed = 'failed'
+}
+
+interface IPaymentRequest extends Ipayment {}
+
+ interface IDataSuccess extends Ipayment {
+    databaseId: number,
+ }
+
+ interface IDataFailed {
+    errorMessage: string,
+    errorCode: number
+ }
+
+ interface IResponseSuccess {
+    status: PaymentStatus.Success,
+    data: IDataSuccess
+ }
+
+ interface IResponseFailed {
+    status: PaymentStatus.Failed,
+    data: IDataFailed
+ }
+
+
+ 
